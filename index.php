3442dd01df21494c839466293ef8c76398dd03b9
Aland Samuel Tiwa <aland_samuel@yahoo.com>
2019-04-16 14:54:16 +0700

    test

--- index.php
+++ index.php
@@ -13,20 +13,23 @@
   <body>
     <h1>Hello, world!</h1>
 
-    <div id="target">
+    <button type="button" id="target" class="btn btn-danger">
         Click here
-      </div>
-      <div id="other">
+    </button>
+      <button type="button" id="other" class="btn btn-warning">
         Trigger the handler
-      </div>
 
+      </btn>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-    <?php echo '<p>testing php</p>'; ?>
+    <?php echo '<p>testing php</p>'; 
+    
+    echo now(); 
+    ?>
 
     <script>
     $( document ).ready(function() {
